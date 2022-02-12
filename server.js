d04c5d7c30334180694b49e1abe48546e0c4be5c
Chaoskrit01 <sukrit-krit-krit-krit@hotmail.com>
2022-02-12 13:24:12 +0700

    Connect with MongoAtlas

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const dotenv = require('dotenv')
-
-const hospitals = require('./routes/hospitals')
+const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'})
 
-const app =express()
+connectDB()
+
+const hospitals = require('./routes/hospitals')
+
+const app = express()
+
+app.use(express.json())
 
 app.use('/api/v1/hospitals', hospitals)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, 
+const server = app.listen(port, 
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 )
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  server.close(() => process.exit(1))
+})
