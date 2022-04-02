c8a084b3872c778cbc49ca345af6efecb9e2bc2b
Chaoskrit01 <sukrit-krit-krit-krit@hotmail.com>
2022-04-02 13:32:39 +0700

    chapter 12 API security

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
 const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'})
@@ -14,6 +20,17 @@ const auth = require('./routes/auth')
 const app = express()
 
 app.use(express.json())
+app.use(mongoSanitize())
+app.use(helmet())
+app.use(xss())
+app.use(hpp())
+app.use(cors())
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 1
+})
+app.use(limiter)
 
 app.use(cookieParser())
 app.use('/api/v1/hospitals', hospitals)
