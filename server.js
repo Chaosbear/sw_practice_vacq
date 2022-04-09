26965a79b666e84d9240491379ad53d38147448e
Chaoskrit01 <sukrit-krit-krit-krit@hotmail.com>
2022-04-09 12:48:14 +0700

    chapter13 openAPI

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const rateLimit = require('express-rate-limit')
 const hpp = require('hpp')
 const cors = require('cors')
 const connectDB = require('./config/db')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
 
 connectDB()
 
@@ -19,6 +21,26 @@ const auth = require('./routes/auth')
 
 const app = express()
 
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Library API',
+      version: '1.0.0',
+      description: 'A simple Express VacQ API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/api/v1',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 app.use(express.json())
 app.use(mongoSanitize())
 app.use(helmet())
@@ -28,7 +50,7 @@ app.use(cors())
 
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 mins
-  max: 1
+  max: 100,
 })
 app.use(limiter)
 
@@ -39,7 +61,8 @@ app.use('/api/v1/auth', auth)
 
 const port = process.env.PORT || 5000
 
-const server = app.listen(port, 
+const server = app.listen(
+  port,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 )
 
